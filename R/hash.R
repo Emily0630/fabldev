32a68540e86d595c9da09ef1549324f03de9b53d
briankundinger <briankundinger@gmail.com>
2023-09-16 15:28:50 -0400

    hash_comparisons

--- R/hash.R
+++ R/hash.R
@@ -1,9 +1,3 @@
-hash_field <- function(L_f, k, Lf_vec){
-  level_seq <- seq_len(L_f)
-  as.numeric(level_seq > 0) * 2 ^ ((level_seq) + (as.numeric(k > 1)  * Lf_vec[k]))
-}
-
-
 #' @export
 #'
 hash_comparisons <- function(comparisons,
@@ -141,3 +135,9 @@ hash_comparisons <- function(comparisons,
   patterns
 
 }
+
+hash_field <- function(L_f, k, Lf_vec){
+  level_seq <- seq_len(L_f)
+  as.numeric(level_seq > 0) * 2 ^ ((level_seq) + (as.numeric(k > 1)  * Lf_vec[k]))
+}
+
